b5b1671cb63a1b92dd5594532802c6f7d9744360
AhMing <keeshengming@outlook.com>
2021-04-05 00:53:13 +0800

    Add simple controller and views

--- routes/web.php
+++ routes/web.php
@@ -25,3 +25,6 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/item', [App\Http\Controllers\ItemsController::class, 'index'])->name('item.home');
+Route::get('/order', [App\Http\Controllers\OrdersController::class, 'index'])->name('order.home');
+Route::get('/setting', [App\Http\Controllers\SettingsController::class, 'index'])->name('setting.home');
