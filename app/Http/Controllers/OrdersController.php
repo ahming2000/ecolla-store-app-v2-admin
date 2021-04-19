ef1c8f5a6863eb140b0f5727501708120dacb296
AhMing <keeshengming@outlook.com>
2021-04-19 16:58:05 +0800

    Add middleware for Order Controller

--- app/Http/Controllers/OrdersController.php
+++ app/Http/Controllers/OrdersController.php
@@ -2,11 +2,21 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Order;
 use Illuminate\Http\Request;
 
 class OrdersController extends Controller
 {
+    /**
+     * OrdersController constructor.
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
     public function index(){
-        return view('order.index');
+        $orders = Order::orderBy('created_at', 'desc')->paginate(5);
+        return view('order.index', compact('orders'));
     }
 }
