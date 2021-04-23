35489d14e9c1b5f285479a5838459e0ebf864fdd
AhMing <keeshengming@outlook.com>
2021-04-23 19:09:39 +0800

    Add order description viewing and button for order item and image receipt

--- resources/views/order/edit.blade.php
+++ resources/views/order/edit.blade.php
@@ -14,14 +14,53 @@
             </div>
         @endif
 
-        <div class="h1">修改订单属性</div>
-        <form action="{{ url('/order/' . $order->id) }}" method="post">
+        <div class="h1">订单 {{ $order->code }} 查看</div>
 
-            @csrf
+        <div class="row">
+            <div class="col-md-6 offset-md-3 col-sm-12 mb-3">
+                <div class="h3">订单详情</div>
+                <table class="table">
+                    <tbody>
+                    <tr>
+                        <td>订单编号：</td>
+                        <td>{{ $order->code }}</td>
+                    </tr>
+                    <tr>
+                        <td>订单模式：</td>
+                        <td>{{ $order->getOrderModeLabel() }}</td>
+                    </tr>
+                    <tr>
+                        <td>付款方式：</td>
+                        <td>{{ $order->payment_method }}</td>
+                    </tr>
+                    <tr>
+                        <td>状态：</td>
+                        <td>{{ $order->getStatusDesc() }}</td>
+                    </tr>
+                    <tr>
+                        @if($order->mode == 'pickup')
+                            <td>电话号码：</td>
+                        @else
+                            <td>邮寄追踪ID：</td>
+                        @endif
+                        <td>{{ $order->delivery_id }}</td>
+                    </tr>
+                    <tr>
+                        <td>邮寄费用：</td>
+                        <td>RM{{ number_format($order->shipping_fee, 2, '.','') }}</td>
+                    </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
 
-            <div class="row">
-                <div class="col-md-6 offset-md-3 col-sm-12 mb-3">
-                    <div class="h3">订单状态</div>
+        <div class="row">
+
+            <div class="col-md-6 offset-md-3 col-sm-12 mb-3">
+                <form action="{{ url('/order/' . $order->id) }}" method="post">
+
+                    @csrf
+                    <div class="h3">更改订单状态</div>
                     <div class="form-check">
                         <input class="form-check-input" type="radio" name="status" id="pending" value="pending">
                         <label class="form-check-label" for="pending">
@@ -53,30 +92,39 @@
                         </label>
                     </div>
 
+                    <div id="additional">
+                        @if($order->mode == 'delivery' && $order->status != 'pending')
+                            <label for="delivery_id">邮寄追踪ID</label>
+                            <input type="text"
+                                   class="form-control @error('delivery_id') is-invalid @enderror"
+                                   name="delivery_id"
+                                   value="{{ old('delivery_id') ?? $order->delivery_id ?? "" }}">
 
-                </div>
-
-                <div class="col-md-6 offset-md-3 col-sm-12 mb-3" id="additional">
-                    @if($order->mode == 'delivery' && $order->status != 'pending')
-                        <label for="delivery_id">邮寄追踪ID</label>
-                        <input type="text"
-                               class="form-control @error('delivery_id') is-invalid @enderror"
-                               name="delivery_id"
-                               value="{{ old('delivery_id') ?? $order->delivery_id ?? "" }}">
-
-                        @error('delivery_id')
-                        <span class="invalid-feedback" role="alert">
+                            @error('delivery_id')
+                            <span class="invalid-feedback" role="alert">
                             <strong>{{ $message }}</strong>
                         </span>
-                        @enderror
-                    @endif
-                </div>
+                            @enderror
+                        @endif
+                    </div>
 
-                <div class="col-12 text-center">
-                    <button type="submit" class="btn btn-primary">保存</button>
+                    <div class="text-center">
+                        <button type="submit" class="btn btn-primary">保存</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-md-6 offset-md-3 col-sm-12 mb-3">
+                <div class="h3">更多</div>
+                <br>
+                <div class="row d-flex justify-content-center">
+                    <a href="{{ url('/order/' . $order->id . '/item') }}" class="btn btn-primary">显示订单物品</a>
+                    <a href="{{ $order->receipt_image }}" class="btn btn-primary">显示订单物品</a>
                 </div>
             </div>
-        </form>
+        </div>
     </main>
 @endsection
 
@@ -96,7 +144,7 @@
             $('input[name=status]').on('change', function () {
                 if ($(this).val() == 'pending') {
                     $('#additional').html('');
-                } else{
+                } else {
                     $('#additional').html(getDeliveryIdLayout());
                 }
             });
