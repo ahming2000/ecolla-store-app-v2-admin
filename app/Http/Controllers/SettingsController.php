23719e8b5a40a299bba76ca7ead504d8fecaf829
AhMing <keeshengming@outlook.com>
2021-04-25 14:54:43 +0800

    Add changing log page

--- app/Http/Controllers/SettingsController.php
+++ app/Http/Controllers/SettingsController.php
@@ -6,7 +6,35 @@ use Illuminate\Http\Request;
 
 class SettingsController extends Controller
 {
+
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
     public function index(){
         return view('setting');
+    }
+
+    public function changingLog(){
+
+        $currentVersion = 'v1.0.1 Beta';
+        $updateDate = '2021/04/23';
+        $color = 'orange'; // Beta: orange, Release: green
+
+        $logs = [
+            [
+                'version' => 'v1.0.1 Beta',
+                'date' => '2021/04/23',
+                'desc' => [
+                    '订单管理更新：可以查看订单详情并可以调整订单状态',
+                    '用户：批发显示功能优化'
+                ]
+            ],
+
+        ];
+
+        $logs = array_reverse($logs);
+        return view('changingLog', compact('currentVersion', 'updateDate', 'color', 'logs'));
     }
 }
---
+++ resources/views/changingLog.blade.php
@@ -0,0 +1,28 @@
+@extends('layouts.app')
+
+@section('title')
+    应用更新日志
+@endsection
+
+@section('content')
+    <main class="container">
+        <div class="row">
+
+            <div class="col-12 text-center">当前版本</div>
+            <div class="col-12 text-center" style="font-size: 30px; color: {{ $color }}">{{ $currentVersion }}</div>
+            <div class="col-12 text-center mb-5">最后更新：{{ $updateDate }}</div>
+
+            <div class="col-md-6 offset-md-3 col-sm-12">
+                <div class="h2 mb-3">更新日志</div>
+                @foreach($logs as $log)
+                    <div class="mb-3">
+                        <div class="h3">{{ $log['version'] }} [{{ $log['date'] }}]</div>
+                        @foreach($log['desc'] as $desc)
+                            <span>- {{ $desc }}</span><br>
+                        @endforeach
+                    </div>
+                @endforeach
+            </div>
+        </div>
+    </main>
+@endsection
