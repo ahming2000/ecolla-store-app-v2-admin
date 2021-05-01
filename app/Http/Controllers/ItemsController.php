a61f610e966a27367ec0b9045364fa49c235a71f
AhMing <keeshengming@outlook.com>
2021-05-02 00:56:14 +0800

    Item edit page completed without variation discount and wholesale discount

--- app/Http/Controllers/ItemsController.php
+++ app/Http/Controllers/ItemsController.php
@@ -76,11 +76,10 @@ class ItemsController extends Controller
             'variations.*.name2' => 'required',
             'variations.*.name1_en' => 'required',
             'variations.*.name2_en' => 'required',
-            'variations.*.barcode' => ['required'],
+            'variations.*.barcode' => 'required',
             'variations.*.price' => 'required',
             'variations.*.weight' => 'required',
-
-            'inventories.*.*.stock' => ''
+            'variations.*.stock' => 'required'
         ]);
 
         $imageData = request()->validate([
@@ -100,41 +99,8 @@ class ItemsController extends Controller
         // Category // TODO - Add default category
         $this->updateCategoryItem($item, array_column($item->categories->toArray(), 'id'), array_column($data['categories'],  'id'));
 
-        // Variation // TODO - Complete the code (now only allow to create new)
-//        foreach($data['variations'] as $v){
-//            $variation = new Variation();
-//            foreach($v as $key => $value){
-//                $variation->setAttribute($key, $value);
-//            }
-//            $item->variations()->save($variation);
-//        }
-
-
-//        for($i = 0; $i < sizeof($item->variations); $i++){
-//            $item->variations[$i]->update($data['variations'][$i]);
-//        }
-        // $this->multipleUpdate('variations', $item->variations->toArray(), $data['variations'], 'barcode', ['item_id' => $item->id]);
-
-        // Inventory // TODO - Complete the code (now only allow to create new)
-//        $currentInventory = array_column($data['inventories'], '0');
-//        for($i = 0; $i < sizeof($item->variations->toArray()); $i++){
-//            $inventory = new Inventory();
-//            $inventory->setAttribute('stock', $currentInventory[$i]['stock']);
-//            $inventory->setAttribute('expire_date', date('Y-m-d'));
-//            $item->variations[$i]->inventories()->save($inventory);
-//        }
-
-
-        // Old inventory
-//        $inventories = [];
-//        foreach($item->variations as $v){
-//            $inventories[] = $v->inventories->toArray();
-//        }
-        // $this->multipleUpdate('inventories', array_column($inventories, '0'),array_column($data['inventories'], '0'),'variation_id');
-
-
-
-
+        // Variation
+        $this->updateVariation($item, $item->variations->toArray(), $data['variations']);
 
         // General Image (completed)
         foreach($imageData['item']['images'] as $img){
@@ -185,9 +151,62 @@ class ItemsController extends Controller
                     ->update(['image'=>$imagePath]);
             }
         }
-        die();
+
+        $item->util()->update(['is_listed' => '1']);
+
         session()->flash('message', '商品已保存并成功上架！');
         header("refresh: 0");
+    }
+
+    private function updateInventory(Item $item, array $old, array $new){
+
+
+    }
+
+    private function updateVariation(Item $item, array $old, array $new){
+        $oldBarcode = array_column($old, 'barcode');
+        $newBarcode = array_column($new, 'barcode');
+
+        $toAddBarcode = array_diff($newBarcode, $oldBarcode);
+        $toDeleteBarcode = array_diff($oldBarcode, $newBarcode);
+        $toUpdateBarcode = array_intersect($newBarcode, $oldBarcode);
+
+        $old = $this->generateArrayKeyFromElement($old, 'barcode');
+        $new = $this->generateArrayKeyFromElement($new, 'barcode');
+
+        // To add
+        foreach($toAddBarcode as $ta){
+            $variation = new Variation();
+            foreach($new[$ta] as $key => $value){
+                $variation->setAttribute($key, $value);
+            }
+            $item->variations()->save($variation);
+        }
+
+        // To delete
+        foreach ($toDeleteBarcode as $td){
+            $id = $old[$td]['id'];
+            Variation::find($id)->delete();
+        }
+
+        // To update
+        foreach ($toUpdateBarcode as $tu){
+            $id = $old[$tu]['id'];
+            DB::table('variations')
+                ->where('id','=',$id)
+                ->update($new[$tu]);
+        }
+
+    }
+
+    private function generateArrayKeyFromElement(array $array, string $arrayKey): array
+    {
+        $temp = [];
+        foreach($array as $arr){
+            $arrayKeyValue = $arr["{$arrayKey}"];
+            $temp[$arrayKeyValue] = $arr;
+        }
+        return $temp;
     }
 
     private function processImage(string $path, $mode = 'frame'){
@@ -253,54 +272,6 @@ class ItemsController extends Controller
         // To remove
         foreach ($toRemove as $tr){
             $item->categories()->detach($tr);
-        }
-    }
-
-    private function multipleUpdate(string $tableName, array $old, array $new, string $primaryKey = 'id', array $foreignKey = [])
-    {
-        // Auto import foreign key to new array
-        if(!empty($foreignKey)){
-            $temp = [];
-            foreach($new as $n){
-                $n = array_merge($n, $foreignKey);
-                $temp[] = $n;
-            }
-            $new = $temp; // Replace
-        }
-
-        $oldId = array_column($old, $primaryKey);
-        $newId = array_column($new, $primaryKey);
-
-        $toAdd = array_diff($newId, $oldId);
-        $toDelete = array_diff($oldId, $newId);
-        $toUpdate = array_intersect($oldId, $newId);
-
-        // To add
-        foreach ($toAdd as $ta) {
-            DB::table($tableName)->insert($this->getArrayByKey($new, $primaryKey, $ta));
-        }
-
-        // To delete
-        foreach ($toDelete as $td) {
-            $id = DB::table($tableName)
-                ->select('id')
-                ->where($primaryKey, '=', $td)
-                ->first()
-                ->id;
-            DB::table($tableName)->delete($id);
-        }
-
-        // To update
-        foreach ($toUpdate as $tu) {
-            $data = $this->getArrayByKey($new, $primaryKey, $tu);
-            foreach($data as $d){
-                $data = $d;
-                break;
-            }
-
-            DB::table($tableName)
-                ->where($primaryKey, '=', $tu)
-                ->update($data);
         }
     }
 
