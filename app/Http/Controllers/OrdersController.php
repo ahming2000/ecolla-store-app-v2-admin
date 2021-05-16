019dc3cc2b56fdd7a57fec48ae2e17e42cdca7db
June <jiajune_lim@live.com>
2021-05-16 21:58:00 +0800

    added export invoice pdf feature

--- app/Http/Controllers/OrdersController.php
+++ app/Http/Controllers/OrdersController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Order;
 use Illuminate\Http\Request;
+use PDF;
 
 class OrdersController extends Controller
 {
@@ -15,29 +16,46 @@ class OrdersController extends Controller
         $this->middleware('auth');
     }
 
-    public function index(){
+    public function index()
+    {
         $orders = Order::orderBy('created_at', 'desc')->paginate(5);
         return view('order.index', compact('orders'));
     }
 
-    public function edit(Order $order){
+    public function edit(Order $order)
+    {
         return view('order.edit', compact('order'));
     }
 
-    public function update(Order $order){
+    public function update(Order $order)
+    {
 
         $status = request('status');
 
-        if($order->mode == 'delivery' && $status != 'pending'){
+        if ($order->mode == 'delivery' && $status != 'pending') {
             $data = request()->validate([
-               'delivery_id' => 'required'
+                'delivery_id' => 'required'
             ]);
             $order->update(['status' => $status, 'delivery_id' => $data['delivery_id']]);
-        } else{
+        } else {
             $order->update(['status' => $status]);
         }
 
         session()->flash('message', '保存成功！');
         header('refresh: 0');
+    }
+
+    // Generate PDF
+    public function createPDF(Order $order)
+    {
+        // retreive all records from db
+        $data = $order;
+
+        // share data to view
+        view()->share('order', $data);
+        $pdf = PDF::loadView('pdf.order', $data);
+
+        // download PDF file with download method
+        return $pdf->download('ecolla-order-' . $order->id . '.pdf');
     }
 }
