bcbe34111bf03d2b1bebc877a0610a4175d4ff7f
AhMing <keeshengming@outlook.com>
2021-05-22 00:38:11 +0800

    Modify logic of checking admin type, add some comment and remove some residual code

--- app/Http/Middleware/PermissionChecker.php
+++ app/Http/Middleware/PermissionChecker.php
@@ -10,19 +10,22 @@ class PermissionChecker
     /**
      * Handle an incoming request.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure  $next
+     * @param Request $request
+     * @param Closure $next
+     * @param $type
      * @return mixed
      */
     public function handle(Request $request, Closure $next, $type)
     {
-
-        if ($request->user()->role == 'employee'){
-            if($type == 'admin'){
+        if ($request->user()->role == 'employee') {
+            // This is used to check the page that is absolutely for admin and it is not available in user permission table
+            if ($type == 'admin') {
                 abort('403', 'You have no permission');
-            }
-            if(!$request->user()->hasAccess($type)){
-                abort('403', 'You have no permission');
+            } else {
+                // Return false also if the type doesn't match any column of user_permissions table
+                if (!$request->user()->hasAccess($type)) {
+                    abort('403', 'You have no permission');
+                }
             }
         }
         return $next($request);
