ba41e3c18780e0842ed4cc14b75ab0a9b9a37a40
AhMing <keeshengming@outlook.com>
2021-05-25 22:25:55 +0800

    Add getAllAttribute function

--- app/Models/UserPermission.php
+++ app/Models/UserPermission.php
@@ -9,6 +9,32 @@ class UserPermission extends Model
 {
     use HasFactory;
 
+    public static function getAllAttributes(): array
+    {
+        return [
+            'item_view',
+            'item_create',
+            'item_update',
+            'item_delete',
+            'item_list',
+            'order_view',
+            'order_update',
+            'order_delete',
+            'order_receipt_view',
+            'order_invoice_download',
+            'order_item_view',
+            'order_item_create',
+            'order_item_update',
+            'order_item_delete',
+            'dashboard_view',
+            'setting_view',
+            'setting_item',
+            'setting_order',
+            'setting_pagination',
+            'setting_account',
+        ];
+    }
+
     public function user(): \Illuminate\Database\Eloquent\Relations\BelongsTo
     {
         return $this->belongsTo(User::class);
