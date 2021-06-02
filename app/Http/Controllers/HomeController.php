e3e40cef4778b2743ed0e958bf7e134ac5faf2fe
AhMing <keeshengming@outlook.com>
2021-06-02 18:12:39 +0800

    Merge master

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -20,6 +20,20 @@ class HomeController extends Controller
         $this->middleware('auth');
     }
 
+    public function performUpdate(){
+        echo 'Category Item attribute update process starting...<br>';
+        DB::table('category_item')->where('category_id', '=', '5')->update(['category_id' => '14']);
+        DB::table('category_item')->where('category_id', '=', '6')->update(['category_id' => '17']);
+        DB::table('category_item')->where('category_id', '=', '8')->update(['category_id' => '19']);
+        DB::table('category_item')->where('category_id', '=', '9')->update(['category_id' => '20']);
+        DB::table('category_item')->where('category_id', '=', '10')->update(['category_id' => '21']);
+        echo 'Category Item attribute update completed.<br>';
+
+
+
+        die('Task completed.');
+    }
+
     /**
      * Show the application dashboard.
      *
