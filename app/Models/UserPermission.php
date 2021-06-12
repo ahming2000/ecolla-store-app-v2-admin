2e7a2c5fee8c3957797ec32539c8f99ad7114afd
AhMing <keeshengming@outlook.com>
2021-06-12 16:25:21 +0800

    Permission grouping data updated

--- app/Models/UserPermission.php
+++ app/Models/UserPermission.php
@@ -40,126 +40,130 @@ class UserPermission extends Model
     public static function getPermissions(): array
     {
         return [
-            'item_view' => [
-                'columnName' => 'item_view',
-                'cnDisplayName' => '商品查看',
-                'elementId' => 'ItemView',
-                'checkedByDefault' => true,
+            'item' => [
+                'item_view' => [
+                    'columnName' => 'item_view',
+                    'cnDisplayName' => '商品查看',
+                    'elementId' => 'ItemView',
+                    'checkedByDefault' => true,
+                ],
+                'item_create' => [
+                    'columnName' => 'item_create',
+                    'cnDisplayName' => '商品创建',
+                    'elementId' => 'ItemCreate',
+                    'checkedByDefault' => false,
+                ],
+                'item_update' => [
+                    'columnName' => 'item_update',
+                    'cnDisplayName' => '商品编辑',
+                    'elementId' => 'ItemUpdate',
+                    'checkedByDefault' => false,
+                ],
+                'item_delete' => [
+                    'columnName' => 'item_delete',
+                    'cnDisplayName' => '商品删除',
+                    'elementId' => 'ItemDelete',
+                    'checkedByDefault' => false,
+                ],
+                'item_list' => [
+                    'columnName' => 'item_list',
+                    'cnDisplayName' => '商品上架',
+                    'elementId' => 'ItemListing',
+                    'checkedByDefault' => false,
+                ],
             ],
-            'item_create' => [
-                'columnName' => 'item_create',
-                'cnDisplayName' => '商品创建',
-                'elementId' => 'ItemCreate',
-                'checkedByDefault' => false,
+            'order' => [
+                'order_view' => [
+                    'columnName' => 'order_view',
+                    'cnDisplayName' => '订单查看',
+                    'elementId' => 'OrderView',
+                    'checkedByDefault' => true,
+                ],
+                'order_update' => [
+                    'columnName' => 'order_update',
+                    'cnDisplayName' => '订单基本属性编辑',
+                    'elementId' => 'OrderUpdate',
+                    'checkedByDefault' => false,
+                ],
+                'order_delete' => [
+                    'columnName' => 'order_delete',
+                    'cnDisplayName' => '订单删除',
+                    'elementId' => 'OrderDelete',
+                    'checkedByDefault' => false,
+                ],
+                'order_receipt_view' => [
+                    'columnName' => 'order_receipt_view',
+                    'cnDisplayName' => '顾客收据查看',
+                    'elementId' => 'OrderReceiptView',
+                    'checkedByDefault' => true,
+                ],
+                'order_invoice_download' => [
+                    'columnName' => 'order_invoice_download',
+                    'cnDisplayName' => '订单详情下载',
+                    'elementId' => 'OrderInvoiceDownload',
+                    'checkedByDefault' => false,
+                ],
+                'order_item' => [
+                    'order_item_view' => [
+                        'columnName' => 'order_item_view',
+                        'cnDisplayName' => '订单商品查看',
+                        'elementId' => 'OrderItemView',
+                        'checkedByDefault' => true,
+                    ],
+                    'order_item_create' => [
+                        'columnName' => 'order_item_create',
+                        'cnDisplayName' => '订单商品创建',
+                        'elementId' => 'OrderItemCreate',
+                        'checkedByDefault' => false,
+                    ],
+                    'order_item_update' => [
+                        'columnName' => 'order_item_update',
+                        'cnDisplayName' => '订单商品编辑',
+                        'elementId' => 'OrderItemUpdate',
+                        'checkedByDefault' => false,
+                    ],
+                    'order_item_delete' => [
+                        'columnName' => 'order_item_delete',
+                        'cnDisplayName' => '订单商品删除',
+                        'elementId' => 'OrderItemDelete',
+                        'checkedByDefault' => false,
+                    ],
+                ],
             ],
-            'item_update' => [
-                'columnName' => 'item_update',
-                'cnDisplayName' => '商品编辑',
-                'elementId' => 'ItemUpdate',
-                'checkedByDefault' => false,
+            'dashboard' => [
+                'dashboard_view' => [
+                    'columnName' => 'dashboard_view',
+                    'cnDisplayName' => '仪表板查看',
+                    'elementId' => 'DashboardView',
+                    'checkedByDefault' => true,
+                ],
             ],
-            'item_delete' => [
-                'columnName' => 'item_delete',
-                'cnDisplayName' => '商品删除',
-                'elementId' => 'ItemDelete',
-                'checkedByDefault' => false,
-            ],
-            'item_list' => [
-                'columnName' => 'item_list',
-                'cnDisplayName' => '商品上架',
-                'elementId' => 'ItemListing',
-                'checkedByDefault' => false,
-            ],
-            'order_view' => [
-                'columnName' => 'order_view',
-                'cnDisplayName' => '订单查看',
-                'elementId' => 'OrderView',
-                'checkedByDefault' => true,
-            ],
-            'order_update' => [
-                'columnName' => 'order_update',
-                'cnDisplayName' => '订单基本属性编辑',
-                'elementId' => 'OrderUpdate',
-                'checkedByDefault' => false,
-            ],
-            'order_delete' => [
-                'columnName' => 'order_delete',
-                'cnDisplayName' => '订单删除',
-                'elementId' => 'OrderDelete',
-                'checkedByDefault' => false,
-            ],
-            'order_receipt_view' => [
-                'columnName' => 'order_receipt_view',
-                'cnDisplayName' => '顾客收据查看',
-                'elementId' => 'OrderReceiptView',
-                'checkedByDefault' => true,
-            ],
-            'order_invoice_download' => [
-                'columnName' => 'order_invoice_download',
-                'cnDisplayName' => '订单详情下载',
-                'elementId' => 'OrderInvoiceDownload',
-                'checkedByDefault' => false,
-            ],
-            'order_item_view' => [
-                'columnName' => 'order_item_view',
-                'cnDisplayName' => '订单商品查看',
-                'elementId' => 'OrderItemView',
-                'checkedByDefault' => true,
-            ],
-            'order_item_create' => [
-                'columnName' => 'order_item_create',
-                'cnDisplayName' => '订单商品创建',
-                'elementId' => 'OrderItemCreate',
-                'checkedByDefault' => false,
-            ],
-            'order_item_update' => [
-                'columnName' => 'order_item_update',
-                'cnDisplayName' => '订单商品编辑',
-                'elementId' => 'OrderItemUpdate',
-                'checkedByDefault' => false,
-            ],
-            'order_item_delete' => [
-                'columnName' => 'order_item_delete',
-                'cnDisplayName' => '订单商品删除',
-                'elementId' => 'OrderItemDelete',
-                'checkedByDefault' => false,
-            ],
-            'dashboard_view' => [
-                'columnName' => 'dashboard_view',
-                'cnDisplayName' => '仪表板查看',
-                'elementId' => 'DashboardView',
-                'checkedByDefault' => true,
-            ],
-            'setting_view' => [
-                'columnName' => 'setting_view',
-                'cnDisplayName' => '设定查看',
-                'elementId' => 'SettingsView',
-                'checkedByDefault' => true,
-            ],
-            'setting_item' => [
-                'columnName' => 'setting_item',
-                'cnDisplayName' => '商品相关设定',
-                'elementId' => 'AccountSettings',
-                'checkedByDefault' => true,
-            ],
-            'setting_order' => [
-                'columnName' => 'setting_order',
-                'cnDisplayName' => '订单相关设定',
-                'elementId' => 'ItemPropertiesSettings',
-                'checkedByDefault' => false,
-            ],
-            'setting_pagination' => [
-                'columnName' => 'setting_pagination',
-                'cnDisplayName' => '显示数量相关设定',
-                'elementId' => 'OrderPropertiesSettings',
-                'checkedByDefault' => false,
-            ],
-            'setting_account' => [
-                'columnName' => 'setting_account',
-                'cnDisplayName' => '账号设置',
-                'elementId' => 'PaginationPropertiesSettings',
-                'checkedByDefault' => false,
-            ],
+            'setting' => [
+                'setting_view' => [
+                    'columnName' => 'setting_view',
+                    'cnDisplayName' => '设定查看',
+                    'elementId' => 'SettingsView',
+                    'checkedByDefault' => true,
+                ],
+                'setting_item' => [
+                    'columnName' => 'setting_item',
+                    'cnDisplayName' => '商品相关设定',
+                    'elementId' => 'ItemPropertiesSettings',
+                    'checkedByDefault' => true,
+                ],
+                'setting_order' => [
+                    'columnName' => 'setting_order',
+                    'cnDisplayName' => '订单相关设定',
+                    'elementId' => 'OrderPropertiesSettings',
+                    'checkedByDefault' => false,
+                ],
+                'setting_account' => [
+                    'columnName' => 'setting_account',
+                    'cnDisplayName' => '账号设置',
+                    'elementId' => 'AccountSettings',
+                    'checkedByDefault' => false,
+                ],
+            ]
         ];
     }
 
