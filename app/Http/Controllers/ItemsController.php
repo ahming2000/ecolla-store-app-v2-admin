e002949c0b10668aa553f0b982530e3b159015eb
AhMing <keeshengming@outlook.com>
2021-07-11 21:28:48 +0800

    Add barcode duplication detection to new controller

--- app/Http/Controllers/ItemsController.php
+++ app/Http/Controllers/ItemsController.php
@@ -23,11 +23,6 @@ class ItemsController extends Controller
         $this->middleware(['auth', 'access:status_check']);
     }
 
-    public function test()
-    {
-
-    }
-
     public function index()
     {
         // Get request value
@@ -94,7 +89,7 @@ class ItemsController extends Controller
         return view('item.create');
     }
 
-    public function store()
+    public function add()
     {
         $data = request()->validate([
             'name' => ['required', 'unique:items']
@@ -126,142 +121,6 @@ class ItemsController extends Controller
 
         return view('item.edit2', compact('item', 'categories'));
     }
-
-//    public function editOld(Item $item)
-//    {
-//        $DEFAULT_CATEGORY_COUNT = SystemConfig::where('name', '=', 'mgmt_i_defaultCategoryCount')->first()->value;
-//        $categories = Category::whereNotBetween('id', [$DEFAULT_CATEGORY_COUNT + 1, 10])->get();
-//        return view('item.edit', compact('item', 'categories'));
-//    }
-
-//    public function updateOld(Item $item)
-//    {
-//        $data = request()->validate([
-//            'item.name' => 'required',
-//            'item.name_en' => '',
-//            'item.desc' => '',
-//            'item.origin' => '',
-//            'item.origin_en' => '',
-//
-//            'categories.*.id' => '',
-//
-//            'variations.*.name' => 'required',
-//            'variations.*.name_en' => '',
-//            'variations.*.barcode' => 'required',
-//            'variations.*.price' => '',
-//            'variations.*.weight' => '',
-//            'variations.*.stock' => ''
-//        ]);
-//
-//        $imageData = request()->validate([
-//            'item.images.*.newImage' => 'image',
-//            'item.images.*.oldImage' => '',
-//            'item.images.*.isEmpty' => '',
-//
-//            'variations.*.barcode' => '',
-//            'variations.*.image' => 'image',
-//            'variations.*.oldImage' => '',
-//            'variations.*.isEmpty' => ''
-//        ]);
-//
-//        // Item
-//        $temp = DB::table('items')
-//            ->select('id')
-//            ->where('name', '=', $data['item']['name'])
-//            ->where('id', '!=', $item->id)
-//            ->first();
-//        if ($temp == null) {
-//            $item->update($data['item']);
-//        } else {
-//            $msgMgr = new MessageManager();
-//            $msgMgr->pushError("此商品名称已被使用！请到<a href=\"/item/" . $temp->id . "/edit\">点击此处</a>添加规格！");
-//            $msgMgr->flashError();
-//            return redirect('/item/' . $item->id . '/edit');
-//        }
-//
-//        // Category
-//        $this->updateCategoryItem($item, array_column($item->categories->toArray(), 'id'), array_column($data['categories'], 'id'));
-//
-//        // Variation
-//        $this->updateVariation($item, $item->variations->toArray(), $data['variations']);
-//
-//        // General Image
-//        foreach ($imageData['item']['images'] as $img) {
-//            if ($img['isEmpty']) {
-//                // Press delete button and left it (Direct delete)
-//                if (isset($img['oldImage'])) {
-//                    $id = DB::table('item_images')
-//                        ->select('id')
-//                        ->where('image', '=', $img['oldImage'])
-//                        ->first()
-//                        ->id;
-//                    DB::table('item_images')->delete($id);
-//                } // else do nothing to ignore the empty image
-//            } else {
-//                if (isset($img['newImage'])) {
-//                    $imagePath = $img['newImage']->store('items/' . $item->id . '');
-//                    $this->processImage(public_path("img/$imagePath"));
-//
-//                    $imagePath = $_SERVER['REQUEST_SCHEME'] . "://" . $_SERVER['SERVER_NAME'] . "/img/" . $imagePath;
-//
-//                    $itemImage = new ItemImage();
-//                    $itemImage->setAttribute('image', $imagePath);
-//                    $item->images()->save($itemImage);
-//
-//                    // Save new image and delete the old one (replace)
-//                    if (isset($img['oldImage'])) {
-//                        $id = DB::table('item_images')
-//                            ->select('id')
-//                            ->where('image', '=', $img['oldImage'])
-//                            ->first()
-//                            ->id;
-//                        ItemImage::find($id)->delete();
-//                    }
-//                } // else do nothing to remain the image
-//            }
-//        }
-//
-//        // Variation Image
-//        foreach ($imageData['variations'] as $v) {
-//            if ($v['isEmpty']) {
-//                // Press delete button and left it (Direct delete)
-//                if (isset($v['oldImage'])) {
-//                    Variation::where('barcode', '=', $v['barcode'])->update(['image' => null]);
-//                } // else do nothing to ignore the empty image
-//            } else {
-//                if (isset($v['image'])) {
-//                    $imagePath = $v['image']->store('items/' . $item->id . '');
-//                    $this->processImage(public_path("img/$imagePath"));
-//
-//                    $imagePath = $_SERVER['REQUEST_SCHEME'] . "://" . $_SERVER['SERVER_NAME'] . "/img/" . $imagePath;
-//
-//                    DB::table('variations')
-//                        ->where('barcode', '=', $v['barcode'])
-//                        ->update(['image' => $imagePath]);
-//
-//                    // Save new image and delete the old one (replace)
-//                    if (isset($v['oldImage'])) {
-//                        $id = DB::table('variations')
-//                            ->where('barcode', '=', $v['barcode'])
-//                            ->update(['image' => null]);
-//                    }
-//                } // else do nothing to remain the image
-//            }
-//        }
-//
-//        // Check the item can be listed or not
-//        if ($this->canList($item->id, true)) {
-//            if (session()->has('error')) {
-//                session()->flash('message', '部分资料已保存！');
-//            } else {
-//                session()->flash('message', '商品资料已保存并成功上架！');
-//            }
-//        } else {
-//            session()->flash('message', '商品资料已保存，但未上架！');
-//        }
-//
-//        return redirect('/item/' . $item->id . '/edit');
-//    }
 
     public function list(Item $item): bool
     {
@@ -360,6 +219,13 @@ class ItemsController extends Controller
 
                 break;
 
+            case "images":
+
+                $data = request('images');
+
+
+                break;
+
             case "category":
 
                 $old = array_column($item->categories->toArray(), 'id');
@@ -378,20 +244,28 @@ class ItemsController extends Controller
 
                     case "add":
 
-                        if ($this->addVariation($item, $data)) {
-                            $msgMgr->pushInfo("添加规格成功！");
+                        if($this->variationBarcodeIsDuplicated($data['barcode'], $item->id)){
+                            $msgMgr->pushError("货号 " . $data['barcode'] . " 已存在！");
                         } else {
-                            $msgMgr->pushError("添加规格失败！请联系技术人员！");
+                            if ($this->addVariation($item, $data)) {
+                                $msgMgr->pushInfo("添加规格成功！");
+                            } else {
+                                $msgMgr->pushError("添加规格失败！请联系技术人员！");
+                            }
                         }
 
                         break;
 
                     case "update":
 
-                        if ($this->updateVariation($data)) {
-                            $msgMgr->pushInfo("规格保存成功！");
+                        if($this->variationBarcodeIsDuplicated($data['barcode'], $item->id)){
+                            $msgMgr->pushError("货号 " . $data['barcode'] . " 已存在！");
                         } else {
-                            $msgMgr->pushError("更新规格失败！请联系技术人员！");
+                            if ($this->updateVariation($data)) {
+                                $msgMgr->pushInfo("规格保存成功！");
+                            } else {
+                                $msgMgr->pushError("更新规格失败！请联系技术人员！");
+                            }
                         }
 
                         break;
@@ -418,8 +292,22 @@ class ItemsController extends Controller
             default:
         }
 
+        if($this->canList($item->id, true)){
+            $msgMgr->pushInfo("商品已自动上架！");
+        } else {
+            $msgMgr->pushInfo("商品未上架！");
+        }
+
         $msgMgr->flashAll();
         return redirect('/item/' . $item->id . '/edit');
+    }
+
+    private function getItem(string $name, string $select = '*')
+    {
+        return DB::table('items')
+            ->selectRaw($select)
+            ->where('name', '=', $name)
+            ->first();
     }
 
     private function addVariation(Item $item, $data): bool
@@ -455,7 +343,6 @@ class ItemsController extends Controller
         return $item->variations()->find($id)->delete();
     }
 
-
     private function itemNameIsDuplicated($id, $name): bool
     {
         $id = DB::table('items')
@@ -467,94 +354,16 @@ class ItemsController extends Controller
         return $id != null;
     }
 
-    private function getItem(string $name, string $select = '*')
+    private function variationBarcodeIsDuplicated($barcode, $item_id): bool
     {
-        return DB::table('items')
-            ->selectRaw($select)
-            ->where('name', '=', $name)
-            ->first();
+        $result = DB::table('variations')
+            ->select('id')
+            ->where('barcode', '=', $barcode)
+            ->where('item_id', '!=', $item_id)
+            ->get();
+
+        return !empty($result->toArray());
     }
-
-
-
-//    private function updateVariation(Item $item, array $old, array $new)
-//    {
-//        // Replace all null value with default value
-//        for ($i = 0; $i < sizeof($new); $i++) {
-//            $new[$i]['price'] = $new[$i]['price'] ?? 0;
-//            $new[$i]['weight'] = $new[$i]['weight'] ?? 0;
-//            $new[$i]['stock'] = $new[$i]['stock'] ?? 0;
-//        }
-//
-//        $oldBarcode = array_column($old, 'barcode');
-//        $newBarcode = array_column($new, 'barcode');
-//
-//        $toAddBarcode = array_diff($newBarcode, $oldBarcode);
-//        $toDeleteBarcode = array_diff($oldBarcode, $newBarcode);
-//        $toUpdateBarcode = array_intersect($newBarcode, $oldBarcode);
-//
-//        $old = $this->generateArrayKeyFromElement($old, 'barcode');
-//        $new = $this->generateArrayKeyFromElement($new, 'barcode');
-//
-//        // To add
-//        foreach ($toAddBarcode as $ta) {
-//            $variation = new Variation();
-//            foreach ($new[$ta] as $key => $value) {
-//                $variation->setAttribute($key, $value);
-//            }
-//
-//            if ($this->variationIsDuplicated($item->variations->toArray(), $variation->barcode, $item->id)) {
-//                Controller::stackError("货号：" . $variation->barcode . " 已存在数据库！");
-//            } else {
-//                $item->variations()->save($variation);
-//            }
-//        }
-//
-//        // To delete
-//        foreach ($toDeleteBarcode as $td) {
-//            $id = $old[$td]['id'];
-//            Variation::find($id)->delete();
-//        }
-//
-//        // To update
-//        foreach ($toUpdateBarcode as $tu) {
-//            $id = $old[$tu]['id'];
-//            DB::table('variations')
-//                ->where('id', '=', $id)
-//                ->update($new[$tu]);
-//        }
-//
-//        if ($this->hasError()) {
-//            $error = Controller::pullError();
-//            session()->flash('error', $error);
-//        }
-//    }
-//
-//    private function processImage(string $path, $mode = 'frame')
-//    {
-//        // $mode can be 'crop'(fit) or 'frame'(resizeCanvas)
-//
-//        $image = Image::make($path);
-//        $min = $image->getWidth() < $image->getHeight() ? $image->getWidth() : $image->getHeight();
-//        $max = $image->getWidth() > $image->getHeight() ? $image->getWidth() : $image->getHeight();
-//
-//        if ($mode == 'crop') {
-//            $image->fit($min);
-//        } else {
-//            if ($image->width() > $max) {
-//                $image->resize($max, null, function ($constraint) {
-//                    $constraint->aspectRatio();
-//                });
-//            }
-//            if ($image->height() > $max) {
-//                $image->resize(null, $max, function ($constraint) {
-//                    $constraint->aspectRatio();
-//                });
-//            }
-//            $image->resizeCanvas($max, $max, 'center', false, '#ffffff');
-//        }
-//        $image->save();
-//    }
 
     private function processCategories(Item $item, array $old, array $new)
     {
@@ -576,12 +385,5 @@ class ItemsController extends Controller
         }
     }
 
-    private function variationIsDuplicated(array $variations, string $barcode, string $item_id): bool
-    {
-        $result = DB::table('variations')
-            ->where('barcode', '=', $barcode)
-            ->where('item_id', '!=', $item_id)
-            ->get();
-        return !empty($result->toArray());
-    }
+
 }
