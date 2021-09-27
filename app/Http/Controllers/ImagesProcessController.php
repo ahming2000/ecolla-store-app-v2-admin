ca0ba06403a4264867b53e1dfeb71972e9a15a43
AhMing <keeshengming@outlook.com>
2021-09-27 11:02:42 +0800

    Server side now detect the file media type to avoid image process error

--- app/Http/Controllers/ImagesProcessController.php
+++ app/Http/Controllers/ImagesProcessController.php
@@ -10,9 +10,29 @@ class ImagesProcessController extends Controller
 {
     public function process(): \Illuminate\Http\JsonResponse
     {
+        $image = request('image');
+
+        // Check extension format (within jpeg, png, gif, webp and bmp)
+        $mediaType = $image->getMimeType(null);
+        switch ($mediaType){
+            case 'image/png':
+            case 'image/gif':
+            case 'image/bmp':
+            case 'image/webp':
+            case 'image/jpeg':
+                break;
+            default:
+                return response()->json([
+                    'image' => '',
+                    'error' => '请使用照片格式 ".jpg", ".png", ".gif", ".webp" 或 ".bmp"！'
+                ]);
+        }
+
+        // Start process
         $mode = request('mode') ?? 'frame';
+
         try{
-            return response()->json(['image' => ImageHandler::processImage(request('image'), $mode)]);
+            return response()->json(['image' => ImageHandler::processImage($image, $mode)]);
         } catch (Exception $exception){
             return response()->json(["image" => "", 'error' => $exception->getMessage()]);
         }
