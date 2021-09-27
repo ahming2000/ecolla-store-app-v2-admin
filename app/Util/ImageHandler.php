150676c9b669a3015930dc647ed2f8ec3097090c
AhMing <keeshengming@outlook.com>
2021-09-27 14:22:11 +0800

    Remove bmp format from allowing image format list

--- app/Util/ImageHandler.php
+++ app/Util/ImageHandler.php
@@ -16,35 +16,44 @@ class ImageHandler
         if (substr($data, 0, 4) == 'http') { // Primary display way (Local)
             return $data;
         } else { // Not readable in string, assume it is binary data
-            // Detect is that png image (fix from last release: data stored did not remove base64 header other than jpg format)
-            $base64 = ImageHandler::binaryToBase64($data);
-            $mediaType = Utility::getStringBetween($base64, 'data', 'base64');
-            switch ($mediaType){
-                case 'image/png': return 'data:image/png;base64,' . substr($base64, 19);
-                case 'image/gif': return 'data:image/gif;base64,' . substr($base64, 19);
-                case 'image/bmp': return 'data:image/bmp;base64,' . substr($base64, 19);
-                default: return 'data:image/jpeg;base64,' . $base64;
-            }
+            return ImageHandler::binaryToBase64($data);
         }
     }
 
-    public static function binaryToBase64($binary)
+    public static function binaryToBase64($binary): string
     {
-        return base64_encode($binary);
+        $base64_temp = base64_encode($binary);
+        // For some reason, replace 'A=' to 'C' fix the png image cannot show properly problem
+        $base64 = substr($base64_temp, 0, -2) . 'C';
+
+        // Detect image format (fix from last release: data stored did not remove base64 header other than jpg format)
+        $mediaType = Utility::getStringBetween($base64, 'data', 'base64');
+        switch ($mediaType){
+            case 'image/jpeg': return 'data:image/jpeg;base64,' . substr($base64, strlen("dataimage/jpegbase64"));
+            case 'image/png': return 'data:image/png;base64,' . substr($base64, strlen("dataimage/pngbase64"));
+            case 'image/gif': return 'data:image/gif;base64,' . substr($base64, strlen("dataimage/gifbase64"));
+            case 'image/webp': return 'data:image/webp;base64,' . substr($base64, strlen("dataimage/webpbase64"));
+            default: return 'data:image/jpeg;base64,' . $base64;
+        }
     }
 
     public static function base64ToBinary($base64)
     {
-        return base64_decode($base64);
+        return base64_decode($base64); // Auto remove character such like ';', ',' and ':'
     }
 
-    public static function convertToLocal(string $base64): string // Pending
+    public static function localToBase64($data): string
+    {
+        return Image::make($data)->encode('data-url')->encoded;
+    }
+
+    public static function convertToLocal(string $base64): string
     {
         if ($base64 == "") return "";
 
         $image = Image::make($base64);
 
-        $mediaType = Utility::getStringBetween($base64, 'data:', ';base64');
+        $mediaType = Utility::getStringBetween($base64, 'data:', ';base64,');
 
         switch ($mediaType){
             case 'image/jpeg': $extension = '.jpg'; break;
@@ -55,11 +64,12 @@ class ImageHandler
         }
 
         $imageName = '';
+        $fileLength = 39; // Use length 39 for converted from base64
         try {
-            $imageName = substr(bin2hex(random_bytes(39)), 0, 40) . $extension;
+            $imageName = substr(bin2hex(random_bytes($fileLength)), 0, $fileLength) . $extension;
         } catch (Exception $ignored) {}
 
-        $image->save("uploads/$imageName");
+        $image->save(public_path("storage/uploads/$imageName"));
         return $_SERVER['REQUEST_SCHEME'] . "://" . $_SERVER['SERVER_NAME'] . ':' . $_SERVER['SERVER_PORT'] . '/storage/uploads/' . $imageName;
     }
 
