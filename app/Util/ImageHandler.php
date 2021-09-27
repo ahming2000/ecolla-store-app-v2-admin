0789cda0e01a6179075707c784e66c902472d9e7
AhMing <keeshengming@outlook.com>
2021-09-27 17:43:12 +0800

    Remove bmp format conversion to local. Refactor new binary-base64 conversion which now support jpeg and png format only.

--- app/Util/ImageHandler.php
+++ app/Util/ImageHandler.php
@@ -20,26 +20,47 @@ class ImageHandler
         }
     }
 
-    public static function binaryToBase64($binary): string
+    public static function binaryToBase64($binary): string // This function only work on jpg and png files
     {
-        $base64_temp = base64_encode($binary);
-        // For some reason, replace 'A=' to 'C' fix the png image cannot show properly problem
-        $base64 = substr($base64_temp, 0, -2) . 'C';
+        $base64 = base64_encode($binary);
+
+        // For some reason, the symbol '=' at the end of the base64 image will cause the image cannot display on the <img> tag correctly
+        $base64 = ImageHandler::removeSymbol($base64);
 
         // Detect image format (fix from last release: data stored did not remove base64 header other than jpg format)
-        $mediaType = Utility::getStringBetween($base64, 'data', 'base64');
+        $mediaType = Utility::getStringBetween($base64, 'MediaType/', '/MediaType');
         switch ($mediaType){
-            case 'image/jpeg': return 'data:image/jpeg;base64,' . substr($base64, strlen("dataimage/jpegbase64"));
-            case 'image/png': return 'data:image/png;base64,' . substr($base64, strlen("dataimage/pngbase64"));
-            case 'image/gif': return 'data:image/gif;base64,' . substr($base64, strlen("dataimage/gifbase64"));
-            case 'image/webp': return 'data:image/webp;base64,' . substr($base64, strlen("dataimage/webpbase64"));
+            case 'image/jpeg': return 'data:image/jpeg;base64,' . substr($base64, strlen("MediaType/image/jpeg/MediaType"));
+            case 'image/png': return 'data:image/png;base64,' . substr($base64, strlen("MediaType/image/png/MediaType"));
+            case 'image/gif': return 'data:image/gif;base64,' . substr($base64, strlen("MediaType/image/gif/MediaType"));
+            case 'image/webp': return 'data:image/webp;base64,' . substr($base64, strlen("MediaType/image/webp/MediaType"));
             default: return 'data:image/jpeg;base64,' . $base64;
         }
     }
 
-    public static function base64ToBinary($base64)
+    public static function removeSymbol($string, $symbol = '='){ // Remove symbol/character from the BACK until do not have same symbol
+        $result = $string;
+        $splitted = str_split($string);
+        for ($i = sizeof($splitted) - 1; $i >= 0; $i--){
+            if ($splitted[$i] == $symbol){
+                $result = substr($result, 0, $i);
+            } else {
+                break;
+            }
+        }
+        return $result;
+    }
+
+    public static function base64ToBinary($base64) // This function only work on jpg and png files
     {
-        return base64_decode($base64); // Auto remove character such like ';', ',' and ':'
+        $mediaType = Utility::getStringBetween($base64, 'data:', ';base64,');
+        switch ($mediaType){
+            case 'image/jpeg': return base64_decode('MediaType/image/jpeg/MediaType' . substr($base64, strlen("data:image/jpeg;base64,")));
+            case 'image/png': return base64_decode('MediaType/image/png/MediaType' . substr($base64, strlen("data:image/png;base64,")));
+            case 'image/gif': return base64_decode('MediaType/image/gif/MediaType' . substr($base64, strlen("data:image/gif;base64,")));
+            case 'image/webp': return base64_decode('MediaType/image/webp/MediaType' . substr($base64, strlen("data:image/webp;base64,")));
+            default: return base64_decode($base64);
+        }
     }
 
     public static function localToBase64($data): string
@@ -57,7 +78,7 @@ class ImageHandler
 
         switch ($mediaType){
             case 'image/jpeg': $extension = '.jpg'; break;
-            case 'image/bmp': $extension = '.bmp'; break;
+            case 'image/webp': $extension = '.webp'; break;
             case 'image/gif': $extension = '.gif'; break;
             case 'image/png': $extension = '.png'; break;
             default: $extension = '';
